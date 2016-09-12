7256312578c493011d70b1ff013b5c937ef9a735
Andrew Cowie <andrew@operationaldynamics.com>
2016-09-12 21:15:17 +1000

    Add listLabels function

--- lib/Shlurp/Operations.hs
+++ lib/Shlurp/Operations.hs
@@ -1,6 +1,8 @@
 module Shlurp.Operations 
 (
-    listIssues
+    listIssues,
+    Label,
+    listLabels
 )
 where
 
@@ -9,9 +11,12 @@ import Data.Maybe (isJust)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 import GitHub.Endpoints.Issues
+import GitHub.Endpoints.Issues.Labels
 import GitHub.Request
 
 import Shlurp.Config
+
+type Label = IssueLabel
 
 listIssues :: GitHub (Vector Issue)
 listIssues = do
@@ -25,6 +30,19 @@ listIssues = do
                     Right issues  -> V.filter (not . isJust . issuePullRequest) issues
                     Left problem  -> error (show problem)
     return issues
+
+listLabels :: GitHub (Vector Label)
+listLabels = do
+    config <- get
+    let token = configToken config
+    let request = labelsOnRepoR (configOwner config) (configRepo config) Nothing
+
+    possibleLabels <- liftIO $ executeRequest token request
+
+    let labels = case possibleLabels of
+                    Right labels  -> labels
+                    Left problem  -> error (show problem)
+    return labels
 
 
 
