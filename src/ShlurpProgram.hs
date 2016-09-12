f33c0bf88134212348cbfc58a51f2ff8d31d0052
Andrew Cowie <andrew@operationaldynamics.com>
2016-09-12 22:45:38 +1000

    Fetch and simple render of issue data

--- src/ShlurpProgram.hs
+++ src/ShlurpProgram.hs
@@ -1,14 +1,32 @@
 module Main where
 
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Vector as V
+import GitHub.Data
 import System.Environment
 
 import Shlurp.Config
+import Shlurp.Operations
 
 main :: IO ()
 main = do
     [owner,repo] <- getArgs
     config <- loadSettings (T.pack owner) (T.pack repo)
-    print owner
-    print repo
-    print config
+
+    issues <- executeGitHub config listIssues
+
+    mapM_ (T.putStrLn . display) issues
+
+display :: Issue -> Text
+display issue =
+  let
+    title = issueTitle issue
+    labels = T.intercalate ", " (V.toList (fmap labelName (issueLabels issue)))
+    description = fromMaybe "~" (issueBody issue)
+  in
+    T.concat [title, "\n", labels, "\n\n", description]
+
+
