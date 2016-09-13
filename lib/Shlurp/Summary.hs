f50694afb77be09044aeb9208b8097a968858326
Andrew Cowie <andrew@operationaldynamics.com>
2016-09-13 11:36:28 +1000

    Rewrite rendering code using ansi-wl-pprint

---
+++ lib/Shlurp/Summary.hs
@@ -0,0 +1,42 @@
+module Shlurp.Summary
+(
+    display
+)
+where
+
+import Prelude hiding ((<$>))
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import GitHub.Endpoints.Issues (Issue, issueLabels, issueTitle, issueBody)
+import GitHub.Endpoints.Issues.Labels (labelName)
+import Text.PrettyPrint.ANSI.Leijen
+
+import Shlurp.Operations (Label)
+
+display :: Vector Issue -> IO ()
+display = putDoc . vsep . fmap renderIssue . V.toList
+
+renderIssue :: Issue -> Doc
+renderIssue issue =
+  let
+    title = renderTitle issue
+    labels = renderLabels issue
+    description = renderBody issue
+  in
+    title <$> labels <$> description
+
+renderLabel :: Label -> Doc
+renderLabel = text . T.unpack . labelName
+
+renderLabels :: Issue -> Doc
+renderLabels = list . fmap renderLabel . V.toList . issueLabels
+
+renderBody :: Issue -> Doc
+renderBody = string . T.unpack . fromMaybe "~" . issueBody
+
+
+renderTitle :: Issue -> Doc
+renderTitle = text . T.unpack . issueTitle
